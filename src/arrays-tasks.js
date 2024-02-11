a0d126d28e65265fb5d3cfee71666574b07117f9
Hanna Kuliaba <hanna.kuliaba@gmail.com>
2024-02-11 16:09:18 +0300

    feat: add solution to the getAverage task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -128,8 +128,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = arr.reduce((accumulator, value) => accumulator + value, 0);
+  const result = sum / arr.length || 0;
+  if (Number.isInteger(result)) {
+    return result;
+  }
+  return Number(result.toFixed(2));
 }
 
 /**
